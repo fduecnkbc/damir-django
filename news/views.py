1bf0860272c8bc4a8520b183a5fab83eca52f99e
fduecnkbc <fduecnkbc@gmail.com>
2017-03-07 10:17:23 +0300

    #4: added news/post_detail.html and news/base.html

--- news/views.py
+++ news/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.order_by('-date')
     return render(request, 'news/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    return render(request, 'news/post_detail.html', {'post': post})
